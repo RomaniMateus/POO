c0811ef137ea976bc60b93d6707fb1412149a0be
MateusRomani <mateus.romani@autaza.com>
2023-09-18 20:29:12 -0300

    Updating repo with the prof implementation

---
+++ Aluguel/index.ts
@@ -0,0 +1,39 @@
+import {App} from "./src/app";
+import {Bike} from "./src/bike";
+import {User} from "./src/user";
+
+const app = new App()
+const bike = new Bike('caloi mountain', 'mountain bike', 100, 200, 150.5, 
+    'My bike', 5, [])
+const bikeId = app.registerBike(bike)
+console.log(app.bikes)
+const user1 = new User('Jose', 'jose@mail.com', '1234')
+const user2 = new User('Maria', 'maria@mail.com', '1234')
+app.registerUser(user1)
+app.registerUser(user2)
+
+const yesterday = new Date()
+yesterday.setDate(yesterday.getDate() - 1)
+const today = new Date()
+const tomorrow = new Date()
+tomorrow.setDate(tomorrow.getDate() + 1)
+const dayAfterTomorrow = new Date()
+dayAfterTomorrow.setDate(dayAfterTomorrow.getDate() + 2)
+const twoDaysFromToday = new Date()
+twoDaysFromToday.setDate(twoDaysFromToday.getDate() + 3)
+
+app.rentBike(bikeId, 'jose@mail.com', yesterday, today)
+
+console.log('Antes do retorno', app.rents)
+
+app.returnBike(bikeId, 'jose@mail.com')
+
+console.log('Depois do retorno', app.rents)
+
+
+
+
+
+
+
+
